458f6a670b99ef051c817736805bcc7318c4d66e
web1n <9633157+web1n@users.noreply.github.com>
2026-03-06 20:29:07 +0800

    Remove localStorage event dispatch

--- src/cloudmusic.d.ts
+++ src/cloudmusic.d.ts
@@ -31,9 +31,6 @@ export type MediaControl = {
     onPlay: (callback: (action: MediaSessionAction) => void) => void;
 };
 
-export type StorageItemChangeEvent = CustomEvent<{ key: string; value: any }>;
-export type StorageItemRemoveEvent = CustomEvent<{ key: string }>;
-
 export type App = {
     getLocalConfig: (request: { type: string; key: string }) => Promise<any>;
     setLocalConfig: (request: { type: string; key: string; value: string }) => void;
--- src/preload/storage.ts
+++ src/preload/storage.ts
@@ -1,25 +1,16 @@
 export function initLocalStorageHook() {
     const originalSetItem = localStorage.setItem;
-    const originalRemoveItem = localStorage.removeItem;
 
     const UPLOAD_CONFIG_KEY_LIST = [
         'setting',
     ];
 
     localStorage.setItem = function (key, value) {
-        const event = new CustomEvent('setStorageItem', { detail: { key, value } });
-        window.dispatchEvent(event);
-
         if (UPLOAD_CONFIG_KEY_LIST.includes(key) && typeof value === 'string') {
             window.App.saveEncryptedConfig(key, value);
         }
 
         originalSetItem.apply(this, arguments);
     };
-    localStorage.removeItem = function (key) {
-        const event = new CustomEvent('removeStorageItem', { detail: { key } });
-        window.dispatchEvent(event);
 
-        originalRemoveItem.apply(this, arguments);
-    };
 }
